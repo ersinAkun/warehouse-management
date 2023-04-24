13a9eb216aae4abf23b104025ead24ce92e50ae3
Ersin Akun <ersinakun34@gmail.com>
2023-04-24 13:36:00 +0300

    first commit

---
+++ src/main/java/com/depo/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.depo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.depo.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long >{
+
+	@EntityGraph(attributePaths = "roles")
+    Optional<User> findByEmail(String email);
+
+}
